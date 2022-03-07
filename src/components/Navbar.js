2b8f0cd8655f958c1fa71c5dcbd9b38177d2693c
kolaso13 <adrbrarare@gmail.com>
2022-03-07 12:58:45 +0100

    v0.101 NavbarCasiAcabado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import "../styles/Navbar.sass";
+import $ from 'jquery';
 
 export const Navbar = () => {
     /*Logica*/
+    $(document).ready(function(){
+      $('#icon').click(function(){
+        $('ul').toggleClass('show')
+      })
+    });
   return (
-    <div>Navbar</div>
+    <nav>
+      <label class="logo">DesignX</label>
+      <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Services</a></li>
+        <li><a href='#'>Contact</a></li>
+        <li><a href='#'>Feedback</a></li>
+      </ul>
+      <label id="icon">
+        <i className='fas fa-bars'></i>
+      </label>
+    </nav>
   )
 }
